f4e6c68cdde77274361ccdc49a0b66470408d386
RajatSinghrs <rajatsingh265@gmail.com>
2025-07-19 19:47:29 +0530

    glass like opaque background

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostListener } from '@angular/core';
 import gsap from 'gsap';
 import ScrollToPlugin from 'gsap/ScrollToPlugin';
 
@@ -13,25 +13,31 @@ gsap.registerPlugin(ScrollToPlugin);
 export class HeaderComponent {
   @Input() isDarkMode = false;
   @Output() themeToggled = new EventEmitter<boolean>();
+  isScrolled = false;
+  scrollTo(sectionId: string) {
+    gsap.to(window, {
+      duration: 1,
+      scrollTo: {
+        y: `#${sectionId}`,
+        offsetY: 80
+      },
+      ease: 'power2.out'
+    });
+  }
 
-scrollTo(sectionId: string) {
-  gsap.to(window, {
-    duration: 1,
-    scrollTo: {
-      y: `#${sectionId}`,
-      offsetY: 80
-    },
-    ease: 'power2.out'
-  });
-}
-
-scrollToTop() {
-  gsap.to(window, {
-    duration: 1,
-    scrollTo: { y: 0 },
-    ease: 'power2.out'
-  });
-}
+  scrollToTop() {
+    gsap.to(window, {
+      duration: 1,
+      scrollTo: { y: 0 },
+      ease: 'power2.out'
+    });
+  }
+  
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.isScrolled = scrollTop > 20 && !this.isDarkMode;
+  }
 
   toggleTheme() {
     this.isDarkMode = !this.isDarkMode;
