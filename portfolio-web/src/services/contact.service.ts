e6496160909cb60e57b22649c024959bc077bd20
RajatSinghrs <rajatsingh265@gmail.com>
2025-08-04 00:25:08 +0530

    api service code

--- portfolio-web/src/services/contact.service.ts
+++ portfolio-web/src/services/contact.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
+  private API = `${environment.apiBaseUrl}/contact`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+     submitContact(data: { name: string; email: string; message: string }) {
+    return this.http.post(this.API, data);
+  }
+  
 }
--- portfolio-web/src/services/feedback.service.ts
+++ portfolio-web/src/services/feedback.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService {
+  private API = `${environment.apiBaseUrl}/feedback`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+   submitFeedback(data: { name: string; rating: number; comment: string }) {
+    return this.http.post(this.API, data);
+  }
+
+  getFeedbacks() {
+    return this.http.get(this.API);
+  }
 }
