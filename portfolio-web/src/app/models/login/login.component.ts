7ada135714b4fed0bcdc703710f2a36c43bef339
RajatSinghrs <rajatsingh265@gmail.com>
2025-08-11 22:49:04 +0530

    login page

--- portfolio-web/src/app/models/login/login.component.ts
+++ portfolio-web/src/app/models/login/login.component.ts
@@ -1,18 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import gsap from 'gsap';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit  {
   email = '';
   password = '';
   error = '';
 
-  constructor(private auth: AuthService, private router: Router) { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object,private auth: AuthService, private router: Router) { }
+
+   ngAfterViewInit() {
+    // Fade & scale in
+      if (isPlatformBrowser(this.platformId)) {
+    gsap.to('.login-container', {
+      duration: 1.2,
+      opacity: 1,
+      scale: 1,
+      ease: 'power3.out'
+    });
+
+    // Pulsing glow
+    gsap.to('.login-container', {
+      boxShadow: '0 0 25px rgba(255,255,255,0.5)',
+      repeat: -1,
+      yoyo: true,
+      duration: 2
+    });
+  }
+   }
+
 
  onLogin() {
     this.auth.login(this.email, this.password).subscribe({
